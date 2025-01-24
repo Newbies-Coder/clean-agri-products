2e61a3f3861ef7967d1928865e020387d72924e3
Nam-th <th.nam1112@gmail.com>
2025-01-24 14:59:50 +0700

    feat(ui): register page

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import RegisterImg from "@/assets/images/Register.png";
+import RegisterForm from "./components/RegisterForm";
+
+const Register = () => {
+  return (
+    <div className="font-[sans-serif] bg-green-900 h-screen">
+      <div className="md:flex items-center gap-8 h-full">
+        <div className="max-md:hidden p-4 md:basis-5/12 lg:basis-6/12 xl:basis-7/12">
+          <img
+            src={RegisterImg}
+            className="lg:max-w-[80%] w-full h-full object-contain block mx-auto"
+            alt="login-image"
+          />
+        </div>
+
+        <div className="md:basis-7/12 lg:basis-6/12 xl:basis-5/12 md:p-7 p-6 flex flex-col justify-center bg-white md:rounded-s-2xl lg:rounded-s-3xl h-full">
+          <div className="max-w-lg w-full mx-auto">
+            <h3 className="text-gray-800 text-4xl font-bold ">
+              Sign up
+            </h3>
+            <p className="text-gray-800 mt-4 mb-16 text-sm">
+              Already have an account?{" "}
+              <Link
+                to="/auth/login"
+                className="text-primary font-semibold hover:underline ml-1 whitespace-nowrap duration-150"
+              >
+                Login here
+              </Link>
+            </p>
+          </div>
+          <RegisterForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
